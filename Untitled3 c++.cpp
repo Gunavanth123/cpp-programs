f0dbd200c469667178ad488e9f31671f49f96a12
Gunavanth123 <113224029+Gunavanth123@users.noreply.github.com>
2022-09-12 10:01:03 +0530

    Add files via upload

---
+++ Untitled3 c++.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int num,r,sum=0,t;
+	printf("input a number:");
+	scanf("%",&num);
+	for(t=num;num!=0;num=num/10){
+		r=num%10;
+		sum=sum*10+r;
+		
+	}
+	printf("the number in reverse order is:%d",sum);
+	
+}
